a1277854c795935e04369438c4349deec38c229e
kennyweigel <kennyweigel@gmail.com>
2019-07-31 19:59:53 -0400

    added user profile association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_16_024648) do
+ActiveRecord::Schema.define(version: 2019_07_31_235215) do
 
   create_table "jwt_blacklists", force: :cascade do |t|
     t.string "jti"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 2019_07_16_024648) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["jti"], name: "index_jwt_blacklists_on_jti"
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "name"
+    t.boolean "is_admin"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
